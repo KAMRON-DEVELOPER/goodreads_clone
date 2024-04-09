68c06b3b9691557f6dc780201efc049520403b64
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-09 08:46:09 +0500

    first commit

--- book/views.py
+++ book/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Book, BookReview, Author
 
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book/books.html')
 
 
