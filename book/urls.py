ecb0adc8a6cdd55490d6cfb6f75df649f90516fd
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-09 09:11:30 +0500

    first commit

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from .views import book_list, book_detail, book_update
 
 
 urlpatterns = [
-    path('', book_list),
+    path('', book_list, name='list'),
     path('<int:id>/', book_detail, name='detail'),
     path('<int:id>/update/', book_update, name='update'),
 ]
