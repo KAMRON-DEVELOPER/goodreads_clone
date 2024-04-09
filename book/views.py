ff17fa5583d85fd9f1bcf5e1c626ae074499ebbc
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-09 08:48:16 +0500

    first commit

--- book/views.py
+++ book/views.py
@@ -4,6 +4,14 @@ from .models import Book, BookReview, Author
 
 def book_list(request):
     books = Book.objects.all()
+    return render(request, 'book/books.html', {'books' : books})
+
+
+def book_detail(request):
+    books = Book.objects.all()
     return render(request, 'book/books.html')
 
 
+def book_update(request):
+    books = Book.objects.all()
+    return render(request, 'book/books.html')
