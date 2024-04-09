eaf3287c512e57403e0c9243dacc56cfba4fc961
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-09 12:07:31 +0500

    first commit

--- book/models.py
+++ book/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from shared.utils import BaseModel
 from users.models import CustomUser
+from django.core.validators import FileExtensionValidator
 
 
 class Author(models.Model):
@@ -19,6 +20,8 @@ class Book(BaseModel):
     description = models.TextField()
     isbn = models.CharField(max_length=17)
     price = models.PositiveIntegerField(default=5000, validators=[MaxValueValidator(1000000)])
+    book_picture = models.ImageField(upload_to='books_pictures/', default='books_pictures/book.png',
+                                       validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
     
     def __str__(self):
         return self.title
