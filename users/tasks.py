1d566e5ed5ff09d253dee79f68d1645c18fd61bb
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-15 12:51:05 +0500

    1

---
+++ users/tasks.py
@@ -0,0 +1,27 @@
+import os
+from celery import Celery
+from django.core.mail import send_mail
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('config')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def send_email(subject, message, recipient_list):
+    send_mail(
+        subject,
+        message,
+        "atajanovkamronbek2003@gmail.com",
+        recipient_list
+    )
