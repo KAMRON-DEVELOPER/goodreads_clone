034af0feaa653a8be6143ae34e3f086907079753
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-15 12:18:15 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -130,4 +130,13 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
+## Broker settings.
+broker_url = 'amqp://guest:guest@localhost:5672//'
 
+# List of modules to import when the Celery worker starts.
+imports = ('myapp.tasks',)
+
+## Using the database to store task state and results.
+result_backend = 'db+sqlite:///results.db'
+
+task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
