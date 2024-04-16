ec601f68db206bdb280e4dcb867f57abd90f05ee
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-16 07:21:47 +0500

    first commit

--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from book.models import BookReview
 from django.core import serializers
+from rest_framework import serializers
+from rest_framework.views import APIView
 
 
 class BookReviewListView(View):
@@ -10,3 +12,10 @@ class BookReviewListView(View):
         book_reviews = BookReview.objects.all()
         json_book_reviews = serializers.serialize('json', BookReview.objects.all())
         return HttpResponse(json_book_reviews, content_type='application/json')
+
+
+class BookReviewAPIView(APIView):
+    pass
+
+
+
