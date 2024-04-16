a61e8f01de606eb0cea33e65ec52024c89b0e2ea
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-16 09:03:47 +0500

    first commit

--- api/views.py
+++ api/views.py
@@ -45,7 +45,12 @@ class BookUpdateApiView(APIView):  # List Update Api
 
 class BookCreateApiView(APIView):  # List Create Api
     def post(self, request):
-        pass
+        book_json = request.data
+        serializered_book = BookSerializer(data=book_json)
+        if serializered_book.is_valid():
+            serializered_book.save()
+            return Response({'created book' : serializered_book.data}, status=status.HTTP_201_CREATED)
+        return Response({'response' : 'book has not created successfully', 'error' : serializered_book.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BookDetailApiView(APIView): # List Detail Api
