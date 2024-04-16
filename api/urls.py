040051d339031938a12a30247fcf3d78501ce940
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-16 07:32:40 +0500

    first commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookReviewListView
+from .views import BookReviewListView, books_list_view
 
 
 urlpatterns = [
-    path('', BookReviewListView.as_view())
+    # path('', BookReviewListView.as_view())
+    path('', books_list_view)
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from book.models import BookReview
+from book.models import Book, BookReview
 from django.core import serializers
 from rest_framework.views import APIView
 from .serializers import BookSerializer, BookReviewSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def books_list_view(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
 
 class BookReviewListView(View):
